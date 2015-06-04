5e8bfe8eb8a4a27d7058193ec0ea5f1053223c61
Ron Chan <ksc.ron@gmail.com>
2015-06-04 21:02:43 +0800

    update Ron 06/04-21:02

--- wp-content/themes/evie/templates/footer.php
+++ wp-content/themes/evie/templates/footer.php
@@ -1,5 +1,29 @@
-<footer class="content-info" role="contentinfo">
+<!--<footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+</footer>-->
+
+
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+  	<div class="row">
+    	<div class="col-xs-12 col-sm-4 col-sm-push-8 col-md-4 col-md-push-8 col-lg-4 col-lg-push-8 social-link-container">
+            area for social media
+		</div>
+	  	<div class="col-xs-12 col-sm-8 col-sm-pull-4 col-md-8 col-md-pull-4  col-lg-8 col-lg-pull-4 footer-link-container">
+	    <?
+		    //$id = array('en'=>4,'zh-hant'=>16,'zh-hans'=>14);
+		    wp_nav_menu(array('menu' => 'footer_navigation','menu_class' => 'footer_link clearfix', 'depth' => 2));
+	    ?>
+		</div>
+	</div>
+    <!--<p class="copyright"><?=get_field("footer_copyright_text",$id[ICL_LANGUAGE_CODE])?></p>-->
+    <div class="copyright">
+    	@2015 EVIE INTERNATIONAL LIMITED. ALL RIGHTS RESERVED 
+    	<span>|</span> <a href="#">Terms of Use</a>
+        <span>|</span> <a href="#">Privacy Policy</a>
+    </div>
+  </div>
 </footer>
+<?php wp_footer(); ?> 
