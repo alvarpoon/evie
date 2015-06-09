53b9e42b060095bba8f1335a63977a386afb1c6f
Ron Chan <ksc.ron@gmail.com>
2015-06-10 00:04:13 +0800

    update Ron 06/10-00:04

--- wp-content/themes/evie/woocommerce/archive-product.php
+++ wp-content/themes/evie/woocommerce/archive-product.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-get_header( 'shop' ); ?>
+//get_header( 'shop' ); ?>
 	<?php
 		/**
 		 * woocommerce_before_main_content hook
@@ -31,18 +31,10 @@ get_header( 'shop' ); ?>
 		<?php endif; ?>
 
 		<?php do_action( 'woocommerce_archive_description' ); ?>
+        
+        <div style="margin-top:10px;">
 
 		<?php if ( have_posts() ) : ?>
-
-			<?php
-				/**
-				 * woocommerce_before_shop_loop hook
-				 *
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
-				 */
-				do_action( 'woocommerce_before_shop_loop' );
-			?>
 
 			<?php woocommerce_product_loop_start(); ?>
 
@@ -70,23 +62,7 @@ get_header( 'shop' ); ?>
 			<?php wc_get_template( 'loop/no-products-found.php' ); ?>
 
 		<?php endif; ?>
-
-	<?php
-		/**
-		 * woocommerce_after_main_content hook
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
-
-	<?php
-		/**
-		 * woocommerce_sidebar hook
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		do_action( 'woocommerce_sidebar' );
-	?>
-
-<?php get_footer( 'shop' ); ?>
+       	
+        </div>
+        
+<?php //get_footer( 'shop' ); ?>
