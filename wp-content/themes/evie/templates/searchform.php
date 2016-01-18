d5c2bd4e1a3a4d554bf7435a6d77737db39ada64
alvarpoon <michiel@sedgwick-richardson.com>
2016-01-19 00:24:27 +0800

    chi comment

--- wp-content/themes/evie/templates/searchform.php
+++ wp-content/themes/evie/templates/searchform.php
@@ -1,9 +1,10 @@
-<form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url(home_url('/')); ?>">
+<form role="search" method="get" class="search-form form-inline" action="<?=icl_get_home_url(); ?>">
   <label class="sr-only"><?php _e('Search for:', 'roots'); ?></label>
   <div class="input-group">
     <input type="search" value="<?php echo get_search_query(); ?>" name="s" class="search-field form-control" placeholder="<?php _e('Search') ?>" required>
     <span class="input-group-btn">
       <button type="submit" class="search-submit btn btn-default"><i class="fa fa-search"></i></button>
+      <input type="hidden" name="lang" value="<?php echo(ICL_LANGUAGE_CODE); ?>"/>
     </span>
   </div>
 </form>
