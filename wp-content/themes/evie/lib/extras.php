2595b7d6808f97db0b6fc54b5068b879bc4325dd
Ron Chan <ksc.ron@gmail.com>
2016-06-21 13:44:12 +0800

    update Ron 21/06/2016-13:44

--- wp-content/themes/evie/lib/extras.php
+++ wp-content/themes/evie/lib/extras.php
@@ -47,3 +47,17 @@ function append_language_class($classes){
 
 // Display 8 products per page. Goes in functions.php
 add_filter( 'loop_shop_per_page', create_function( '$cols', 'return 8;' ), 20 );
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
